0e9e2574a0b4cdc6c24442d3873c9345f608ece5
Russian Reboucas <russian.reboucas@gmail.com>
2017-01-02 00:24:45 -0300

    clojure/hangman: define a function

--- clojure/hangman/src/hangman/core.clj
+++ clojure/hangman/src/hangman/core.clj
@@ -1,6 +1,20 @@
 (ns hangman.core
   (:gen-class))
 
+(def total-lifes 6)
+
+(defn loose [] (print "You loose!"))
+
+(defn run [lifes]
+  (if (= lifes 0)
+    (loose)
+    (do
+      (print lifes)
+      (run (dec lifes))
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
